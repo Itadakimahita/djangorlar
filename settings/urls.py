b86189d87189a7897d8e7d31934b7f77b7ab07c6
Dias <kadirov.dias@mail.ru>
2025-09-29 23:37:08 +0500

    welcome page

--- settings/urls.py
+++ settings/urls.py
@@ -7,7 +7,7 @@ from apps.views import hello_view, users_info, city_time, counter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(route="hello/", view=hello_view, name="hello-view"),
+    path(route="", view=hello_view, name="hello-view"),
     path(route="users/", view=users_info, name="users"),
     path(route="cities/", view=city_time, name="city-time"),
     path(route="counter/", view=counter, name="counter"),
