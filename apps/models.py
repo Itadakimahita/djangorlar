fb4c670d287b437d67d70a9536070806e587f2db
Dias <kadirov.dias@mail.ru>
2025-11-03 23:30:39 +0500

    init 6 practice

---
+++ apps/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class CustomUser(User):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(_('active'), default=True)
