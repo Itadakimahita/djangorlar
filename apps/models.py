93e2895da6dbe4fefcc57bd194b5a487e563eb86
Dias <kadirov.dias@mail.ru>
2025-11-03 23:44:04 +0500

    email change

--- apps/models.py
+++ apps/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class CustomUser(User):
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.CharField(_('email address'), max_length=255, unique=True)
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
